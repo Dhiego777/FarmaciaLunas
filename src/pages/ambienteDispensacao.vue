731452e9f55fc082431466d856af33b2f5c9c1ca
Dhiego777 <dhiego-cavalcante@hotmail.com>
2021-06-03 11:11:03 -0300

    Adição do VIA CEP

--- src/pages/ambienteDispensacao.vue
+++ src/pages/ambienteDispensacao.vue
@@ -1,96 +1,174 @@
 <template>
-  <div class="row" style="margin: auto">
-    <div class="col-12" style="padding-left: 100px; margin-right: -10px">
-      <div class="row w-75">
-        <div class="col">
-          <label class="form-label">CPF</label>
-          <input
-            type="text"
-            class="form-control"
-            aria-describedby="cpfHelp"
-            placeholder="Digite aqui o CPF"
-            v-model="cpf"
-          />
+  <nav class="mt-3 mb-5">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+    </ul>
+  </nav>
+  <div class="container-fluid">
+    <div class="row d-flex justify-content-center">
+      <div class="col-6 mb-3">
+        <label class="form-label">CPF</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Digite aqui o CPF"
+          v-model="cpf"
+        />
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-6 mb-3">
+        <label class="form-label">Nome</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Digite aqui o Nome"
+          v-model="nome"
+        />
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-3 mb-3">
+        <label for="sexo">Sexo</label>
+        <form>
+          <select v-model="sexo" name="sexo" id="sexo">
+            <option value="-1" disabled>Selecione o Sexo</option>
+            <option value="m">Masculino</option>
+            <option value="f">Feminino</option>
+          </select>
+        </form>
+      </div>
+      <div class="col-3 mb-3">
+        <label class="form-label">Idade</label>
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Digite aqui a Idade"
+          v-model="idade"
+        />
+      </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-6 mb-3">
+        <label class="form-label">CEP</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Digite aqui o CEP"
+          v-model="cep"
+        />
+        <div class="d-grid gap-2 mt-2">
+          <button
+            type="button"
+            @click="getDetailsFromViaCep()"
+            class="btn btn-primary btn-block"
+          >
+            Pesquisar
+          </button>
         </div>
       </div>
-      <div class="row w-75">
-        <div class="col">
-          <label class="form-label">Nome</label>
-          <input
-            type="text"
-            class="form-control"
-            placeholder="Digite aqui o Nome"
-            v-model="nome"
-          />
-        </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-6 mb-3">
+        <label class="form-label">Rua</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Digite aqui o Endereço"
+          v-model="place.logradouro"
+        />
       </div>
-      <div class="row w-75">
-        <div class="col">
-          <label class="form-label">Idade</label>
-          <input
-            type="number"
-            class="form-control"
-            placeholder="Digite aqui a Idade"
-            v-model="idade"
-          />
-        </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-3 mb-3">
+        <label class="form-label">Bairro</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Digite aqui o Bairro"
+          v-model="place.bairro"
+        />
       </div>
-      <div class="row w-75">
-        <div class="col">
-          <label class="form-label">Endereço</label>
-          <input
-            type="number"
-            class="form-control"
-            placeholder="Digite aqui a Idade"
-            v-model="idade"
-          />
-        </div>
+      <div class="col-3 mb-3">
+        <label class="form-label">Cidade</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Digite aqui o Cidade"
+          v-model="place.localidade"
+        />
       </div>
-      <div class="row w-75">
-        <div class="col">
-          <label class="form-label">Telefone</label>
-          <!-- Fazer dois campos para o ddd e o telefone  -->
-          <input
-            type="number"
-            class="form-control"
-            placeholder="Digite aqui a Idade"
-            v-model="idade"
-          />
-        </div>
+    </div>
+    <div class="row d-flex justify-content-center">
+      <label class="form-label">Telefone</label>
+      <div class="col-3 mb-3">
+        <label class="form-label">DDD</label>
+        <input
+          type="number"
+          class="form-control"
+          placeholder="DDD"
+          v-model="telefone.ddd"
+        />
       </div>
-      <div class="row w-75">
-        <div class="col">
-          <label class="form-label">CEP</label>
-          <input
-            type="number"
-            class="form-control"
-            placeholder="Digite aqui a Idade"
-            v-model="idade"
-          />
-        </div>
+      <div class="col-3 mb-3">
+        <label class="form-label">Telefone</label>
+        <input
+          type="number"
+          class="form-control"
+          placeholder="Digite aqui a telefone"
+          v-model="telefone.numero"
+        />
       </div>
-      <div class="row w-75">
-        <div class="col">
-          <label class="form-label">Sexo</label>
-          <input
-            type="number"
-            class="form-control"
-            placeholder="Digite aqui a Idade"
-            v-model="idade"
-          />
+    </div>
+    <div class="row d-flex justify-content-center">
+      <div class="col-6 mb-3">
+        <div class="d-grid gap-2 mt-2">
+          <button type="button" class="btn btn-primary btn-block">
+            Cadastrar
+          </button>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+//import axios
+
 export default {
-  data: function () {
+  data() {
     return {
-      cpf: this.cpf,
-      nome: this.nome,
-      idade: this.idade,
+      cpf: 0,
+      nome: "",
+      idade: 0,
+      sexo: "-1",
+      cep: "",
+      place: {
+        logradouro: "",
+        bairro: "",
+        localidade: "",
+      },
+      telefone: {
+        ddd: "",
+        numero: "",
+      },
     };
+  },
+  props: {
+    msg: String,
+  },
+  methods: {
+    getDetailsFromViaCep: function () {
+      // HTTP get https://viacep.com.br/ws/${this.cep}/json/
+      fetch(`https://viacep.com.br/ws/${this.cep}/json/`)
+        .then((response) => {
+          response.json().then((json) => {
+            this.place = json;
+          });
+        })
+        .catch((e) => {
+          alert(e);
+        });
+    },
   },
 };
 </script>
